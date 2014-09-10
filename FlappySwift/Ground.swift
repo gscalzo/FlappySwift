1a2e43dba2a300ed974ef24bbf79a800051546f7
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-09-10 09:30:32 +0100

    extracted builder closure

--- FlappySwift/Ground.swift
+++ FlappySwift/Ground.swift
@@ -27,16 +27,25 @@ class Ground  {
         let groundBody = SKNode()
         groundBody.position = CGPoint(x: width/2.0, y: height/2)
         
-        groundBody.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: width, height: height))
-        groundBody.physicsBody!.dynamic = false
-        groundBody.physicsBody!.affectedByGravity = false
-        groundBody.physicsBody!.categoryBitMask = BodyType.world.toRaw()
-        groundBody.physicsBody!.collisionBitMask = BodyType.world.toRaw()
+        //        groundBody.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: width, height: height))
+        //        groundBody.physicsBody!.dynamic = false
+        //        groundBody.physicsBody!.affectedByGravity = false
+        //        groundBody.physicsBody!.categoryBitMask = BodyType.world.toRaw()
+        //        groundBody.physicsBody!.collisionBitMask = BodyType.world.toRaw()
+
+        groundBody.physicsBody = SKPhysicsBody.rectSize(CGSize(width: width, height: height)){ body in
+            body.dynamic = false
+            body.affectedByGravity = false
+            body.categoryBitMask = BodyType.world.toRaw()
+            body.collisionBitMask = BodyType.world.toRaw()
+        }
+        
         parentNode.addChild(groundBody)
         return self
     }
 }
 
+// Startable
 extension Ground : Startable {
     func start() -> Startable {
         parallaxNode.start(duration: 5.0)
