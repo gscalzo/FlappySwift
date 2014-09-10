a6de6f4e47a1c1f1885dcecf7a2755e866062526
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-09-10 14:03:04 +0100

    step03

---
+++ FlappySwift/Bird.swift
@@ -0,0 +1,93 @@
+//
+//  Bird.swift
+//  FlappySwift
+//
+//  Created by Giordano Scalzo on 03/06/2014.
+//  Copyright (c) 2014 Effective Code. All rights reserved.
+//
+
+import SpriteKit
+
+class Bird : Startable {
+    private let node: SKSpriteNode!
+    private let textureNames: [String]
+    
+    var position : CGPoint {
+        set { node.position = newValue }
+        get { return node.position }
+    }
+    
+    init(textureNames: [String]) {
+        self.textureNames = textureNames
+        node = createNode()
+    }
+    
+    func addTo(scene: SKSpriteNode) -> Bird{
+        scene.addChild(node)
+        return self
+    }
+}
+
+// Creators
+extension Bird {
+    private func createNode() -> SKSpriteNode {
+        let birdNode = SKSpriteNode(imageNamed: textureNames.first!)
+        birdNode.setScale(1.8)
+        birdNode.zPosition = 2.0
+        
+        birdNode.physicsBody = SKPhysicsBody(rectangleOfSize: birdNode.size)
+        birdNode.physicsBody!.dynamic = true
+
+        return birdNode
+    }
+    
+}
+
+// Startable
+extension Bird : Startable {
+    func start() -> Startable {
+        animate()
+        return self
+    }
+    
+    func stop() -> Startable {
+        node.physicsBody!.dynamic = false
+        node.removeAllActions()
+        return self
+    }
+}
+
+// Actions
+extension Bird {
+    func update() {
+        switch node.physicsBody!.velocity.dy {
+        case let dy where dy > 30.0:
+            node.zRotation = (3.14/6.0)
+        case let dy where dy < -100.0:
+            node.zRotation = -1*(3.14/4.0)
+        default:
+            node.zRotation = 0.0
+        }
+    }
+    
+    func flap() {
+        node.physicsBody!.velocity = CGVector(dx: 0, dy: 0)
+        node.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 6))
+    }
+}
+
+// Private
+extension Bird {
+    private func animate(){
+        let animationFrames = textureNames.map { texName in
+            SKTexture(imageNamed: texName)
+        }
+        
+        node.runAction(
+            SKAction.repeatActionForever(
+                SKAction.animateWithTextures(animationFrames, timePerFrame: 0.1)
+            ))
+    }
+}
+
+
--- FlappySwift/GameScene.swift
+++ FlappySwift/GameScene.swift
@@ -11,15 +11,20 @@ import SpriteKit
 class GameScene: SKScene {
     private var screenNode: SKSpriteNode!
     private var actors: [Startable]!
+    private var bird: Bird!
 
     override func didMoveToView(view: SKView) {
-        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -3)
+
         screenNode = SKSpriteNode(color: UIColor.clearColor(), size: self.size)
         addChild(screenNode)        
         
         let bg = Background(textureNamed: "background").addTo(screenNode)
         let te = Ground(textureNamed: "ground").addTo(screenNode)
-        actors = [bg, te]
+        bird = Bird(textureNames: ["bird1", "bird2"]).addTo(screenNode)
+        bird.position = CGPointMake(30.0, 400.0)
+
+        actors = [bg, te, bird]
         
         for actor in actors {
             actor.start()
@@ -27,10 +32,12 @@ class GameScene: SKScene {
     }
     
     override func update(currentTime: CFTimeInterval) {
+        bird.update()
     }
     
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        bird.flap()
     }
 }
 
