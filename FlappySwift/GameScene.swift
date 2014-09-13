d97f35d96fcfaa8312ed62153668c1496f14a735
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-09-13 15:20:53 +0100

    removed unused files

--- FlappySwift/GameScene.swift
+++ FlappySwift/GameScene.swift
@@ -10,20 +10,13 @@ import SpriteKit
 
 class GameScene: SKScene {
     private var screenNode: SKSpriteNode!
-    private var actors: [Startable]!
 
     override func didMoveToView(view: SKView) {
-        
         screenNode = SKSpriteNode(color: UIColor.clearColor(), size: self.size)
         addChild(screenNode)        
         
-        let bg = Background(textureNamed: "background").addTo(screenNode)
-        let te = Ground(textureNamed: "ground").addTo(screenNode)
-        actors = [bg, te]
-        
-        for actor in actors {
-            actor.start()
-        }
+        Background(textureNamed: "background").addTo(screenNode).start()
+        Ground(textureNamed: "ground").addTo(screenNode).start()
     }
     
     override func update(currentTime: CFTimeInterval) {
