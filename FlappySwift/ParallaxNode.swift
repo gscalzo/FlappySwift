266a7943c2dab4cfe5a153bd94157fe7a3a62f50
giordano scalzo <giordano.scalzo@gmail.com>
2015-02-23 01:12:42 +0000

    removed unused imports

--- FlappySwift/ParallaxNode.swift
+++ FlappySwift/ParallaxNode.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 Effective Code. All rights reserved.
 //
 
-import UIKit
 import SpriteKit
 
 class ParallaxNode {
--- FlappySwift/Startable.swift
+++ FlappySwift/Startable.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 Effective Code. All rights reserved.
 //
 
-import Foundation
 import SpriteKit
 
 protocol Startable {
