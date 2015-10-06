537b9485c2db9a0f94ee00f6f997e5bb847ad1a7
Giordano Scalzo <giordano.scalzo@gmail.com>
2015-10-06 23:27:16 +0100

    Updated to Xcode 7

--- FlappySwift/GameScene.swift
+++ FlappySwift/GameScene.swift
@@ -14,12 +14,13 @@ class GameScene: SKScene {
 
     override func didMoveToView(view: SKView) {
         screenNode = SKSpriteNode(color: UIColor.clearColor(), size: self.size)
+        screenNode.anchorPoint = CGPoint(x: 0, y: 0)
         addChild(screenNode)
-        let sky = Background(textureNamed: "sky", duration:60.0).addTo(screenNode)
-        let city = Background(textureNamed: "city", duration:20.0).addTo(screenNode)
-        let ground = Background(textureNamed: "ground", duration:5.0).addTo(screenNode)
+        let sky = Background(textureNamed: "sky", duration:60.0).addTo(screenNode, zPosition: 0)
+        let city = Background(textureNamed: "city", duration:20.0).addTo(screenNode, zPosition: 1)
+        let ground = Background(textureNamed: "ground", duration:5.0).addTo(screenNode, zPosition: 2)
         actors = [sky, city, ground]
-        
+
         for actor in actors {
             actor.start()
         }
