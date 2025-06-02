ce40340aa313c75d699feffa3375ea97cf31ae27
giordano scalzo <giordano.scalzo@gmail.com>
2025-06-02 20:36:42 +0100

    Refactor MenuViewController and GameViewController by removing unused Game Center button and related code. Cleaned up the MenuViewController to enhance readability and maintainability.

--- FlappySwift/GameViewController.swift
+++ FlappySwift/GameViewController.swift
@@ -9,37 +9,6 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(_ file: String) throws -> GameScene? {
-        if let path = Bundle.main.path(forResource: file, ofType: "sks") {
-            let sceneData = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-            let archiver = try NSKeyedUnarchiver(forReadingFrom: sceneData)
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let decoded = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey)
-            archiver.finishDecoding()
-            if let scene = decoded as? GameScene {
-                print("[GameViewController] Successfully decoded a GameScene from SKS.")
-                return scene
-            } else {
-                print("[GameViewController] Decoded object from SKS is not a GameScene. Actual type: \(type(of: decoded))")
-                let mirror = Mirror(reflecting: decoded as Any)
-                print("[GameViewController] Properties of decoded object:")
-                for child in mirror.children {
-                    if let label = child.label {
-                        print("  \(label): \(child.value)")
-                    } else {
-                        print("  (no label): \(child.value)")
-                    }
-                }
-                return nil
-            }
-        } else {
-            print("[GameViewController] Could not find SKS file named \(file).sks in bundle.")
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
     private let skView = SKView()
     var gameCenter: GameCenter?
--- FlappySwift/MenuViewController.swift
+++ FlappySwift/MenuViewController.swift
@@ -11,13 +11,10 @@ import HTPressableButton
 
 class MenuViewController: UIViewController {
     private let playButton = HTPressableButton(frame: CGRect(x: 0, y: 0, width: 260, height: 50), buttonStyle: .rect)!
-    private let gameCenterButton = HTPressableButton(frame: CGRect(x: 0, y: 0, width: 260, height: 50), buttonStyle: .rect)!
     private var player: MusicPlayer?
-    private let gameCenter = GameCenter()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        gameCenter.authenticateLocalPlayer()
         do {
              player = try MusicPlayer(filename: "Pamgaea", type: "mp3")
              player!.play()
@@ -38,21 +35,16 @@ private extension MenuViewController {
         playButton.addTarget(self, action: #selector(onPlayPressed(_:)), for: .touchUpInside)
         playButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(playButton)
-        gameCenterButton.addTarget(self, action: #selector(onGameCenterPressed(_:)), for: .touchUpInside)
-        gameCenterButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(gameCenterButton)
     }
     
     @objc func onPlayPressed(_ sender: UIButton) {
         let vc = GameViewController()
-        vc.gameCenter = gameCenter
         vc.modalTransitionStyle = .crossDissolve
         present(vc, animated: true, completion: nil)
     }
     
     @objc func onGameCenterPressed(_ sender: UIButton) {
         print("onGameCenterPressed")
-        gameCenter.showLeaderboard()
     }
 }
 
@@ -66,13 +58,6 @@ extension MenuViewController {
             playButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
             playButton.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -60)
         ])
-        // Game Center Button Constraints
-        NSLayoutConstraint.activate([
-            gameCenterButton.heightAnchor.constraint(equalToConstant: 80),
-            gameCenterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            gameCenterButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
-            gameCenterButton.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: 60)
-        ])
     }
 }
 
@@ -81,8 +66,6 @@ private extension MenuViewController {
     func style() {
         playButton.buttonColor = UIColor.ht_grapeFruit()
         playButton.shadowColor = UIColor.ht_grapeFruitDark()
-        gameCenterButton.buttonColor = UIColor.ht_aqua()
-        gameCenterButton.shadowColor = UIColor.ht_aquaDark()
     }
 }
 
@@ -90,7 +73,6 @@ private extension MenuViewController {
 private extension MenuViewController {
     func render() {
         playButton.setTitle("Play", for: .normal)
-        gameCenterButton.setTitle("Game Center", for: .normal)
     }
 }
 
